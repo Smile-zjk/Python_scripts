cd35050de848afe4124794a0d3a9eb9d291bdc65
Casuall <Smile_zjk@qq.com>
2020-04-05 21:05:18 +0800

    selection sort

---
+++ Algorithms/selection_sort.py
@@ -0,0 +1,19 @@
+def findSmallest(arr: list) -> int :
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if smallest > arr[i]:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr: list) -> list:
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectionSort([2, 3, 8, 4, 9, 6]))
+
+
